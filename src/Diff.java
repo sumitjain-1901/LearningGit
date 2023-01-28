9ab3c75e46107c856fbc04f0370e428489172eaa
Sumit Jain <sumitjain.1911998@gmail.com>
2023-01-28 22:18:31 +0530

    adding implementation to Diff.java

--- src/Diff.java
+++ src/Diff.java
@@ -1,2 +1,8 @@
 public class Diff {
+    public static void main(String[] args) {
+        int num1 = 10;
+        int num2 = 20;
+        int diff = num1 - num2;
+        System.out.println("Difference is = "+diff);
+    }
 }

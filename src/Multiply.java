994aa520b460c5794b2e300a3c5e496ab57a9033
Sumit Jain <sumitjain.1911998@gmail.com>
2023-01-28 21:32:44 +0530

    Added Multiply.java

---
+++ src/Multiply.java
@@ -0,0 +1,10 @@
+public class Multiply {
+
+    public static void main(String[] args)
+    {
+        int num1 = 10;
+        int num2 = 20;
+        int product = num1 * num2;
+        System.out.println("Product is = "+product);
+    }
+}

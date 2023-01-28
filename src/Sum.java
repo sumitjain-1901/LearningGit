78cefff4cc12ee2f6295a5fead5e758f22686a96
Sumit Jain <sumitjain.1911998@gmail.com>
2023-01-28 21:13:44 +0530

    adding message in Sum.java

--- src/Sum.java
+++ src/Sum.java
@@ -4,6 +4,6 @@ public class Sum {
         int num1 = 10;
         int num2 = 20;
         int sum = num1 + num2;
-        System.out.println(sum);
+        System.out.println("Sum is = "+sum);
     }
 }

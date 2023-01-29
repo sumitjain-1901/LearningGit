b67d327cfa757ad2e5ebc94a5c18b0cd3574a822
Sumit Jain <sumitjain.1911998@gmail.com>
2023-01-29 11:23:13 +0530

    aading modulo.java

---
+++ src/Modulo.java
@@ -0,0 +1,8 @@
+public class Modulo {
+    public static void main(String[] args) {
+        int num1 = 10;
+        int num2 = 20;
+        int modulo = num1 % num2;
+        System.out.println("Modulo is = "+modulo);
+    }
+}

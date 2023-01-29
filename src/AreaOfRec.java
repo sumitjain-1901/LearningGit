2347cb966b6a53ba368dfa2e0490fb106acb8c51
Sumit Jain <sumitjain.1911998@gmail.com>
2023-01-29 11:27:39 +0530

    aading Area.java

---
+++ src/AreaOfRec.java
@@ -0,0 +1,8 @@
+public class AreaOfRec {
+    public static void main(String[] args) {
+        int num1 = 10;
+        int num2 = 20;
+        int area = num1 * num2;
+        System.out.println("Area is = "+area);
+    }
+}

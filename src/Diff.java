6cfd198a6d9ae0ae2255383713319174c737c726
Sumit Jain <sumitjain.1911998@gmail.com>
2023-01-29 13:01:39 +0530

    Modifying diff

--- src/Diff.java
+++ src/Diff.java
@@ -2,7 +2,14 @@ public class Diff {
     public static void main(String[] args) {
         int num1 = 10;
         int num2 = 20;
-        int diff = num1 - num2;
+        int diff = 0;
+        if(num1 > num2)
+        {
+            diff = num1 - num2;
+        }
+        else{
+            diff = num2 - num1;
+        }
         System.out.println("Difference is = "+diff);
     }
 }

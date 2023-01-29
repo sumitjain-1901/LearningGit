081f8a98146d333844827575b90dc261d3e9da22
Sumit Jain <sumitjain.1911998@gmail.com>
2023-01-29 19:33:52 +0530

    Correcting msg in Product.java

--- src/Multiply.java
+++ src/Multiply.java
@@ -5,6 +5,6 @@ public class Multiply {
         int num1 = 10;
         int num2 = 20;
         int product = num1 * num2;
-        System.out.println("Product is = "+product);
+        System.out.println("Product are = "+product);
     }
 }

1714f3d418080284709ee73ba6eda6ce6bbd1998
Sumit Jain <sumitjain.1911998@gmail.com>
2023-01-29 21:33:21 +0530

    Multiply

--- src/Multiply.java
+++ src/Multiply.java
@@ -4,7 +4,9 @@ public class Multiply {
     {
         int num1 = 10;
         int num2 = 20;
-        int product = num1 * num2;
-        System.out.println("Product are = "+product);
-    }
+        int num3 = 30;
+        int product = num1 * num2 * num3;
+        System.out.println("Product of 3 number = "+product);
+
+       }
 }
